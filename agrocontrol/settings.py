7f8dea3b4c3fdb2305eb7447389d44b6d63eb624
Yashdotpy <yashwalhekar545@gmail.com>
2026-01-13 18:39:49 +0530

    Set DEBUG false for production

--- agrocontrol/settings.py
+++ agrocontrol/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lj(*7!c6$*6x9%92orh1xgk4yr&vsgkqd@onny_*$=+%8h5n$3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
